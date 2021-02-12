995f4589f13ef1bd7da2c6877e7229b521b412f5
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-02-13 10:31:36 +1100

    Updated to 1.1.0
    - Notifications are now optional
    - You can now set custom keybinds
    - You can now decrement the total
    - Decrement Value is customisable
    - Made prompts neater

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-import keyboard
+import keyboard, time
 from win10toast import ToastNotifier
 
 template = input("What are you counting? ")
-count = int(input("What is the increment value? "))
+print("")
+countI = int(input("What is the increment value? "))
+countD = int(input("What is the decrement value? "))
+print("")
+notify = input("Would you like a notification to appear when you increment or decrement the value? ")
+print("")
+keybindUp = input("Please enter a keybind to increment: ")
+keybindDown = input("Please enter a keybind to decrement: ")
+
+if notify.lower() == "yes" or notify.lower() == "y":
+    notify = True
+    print("a")
+else:
+    notify = False
+
 value = 0
 
 while True:
-    if keyboard.is_pressed("ctrl+l"):
-        value += count
+    if keyboard.is_pressed(keybindUp):
+        value += countI
         file = open("counter.txt", "w")
         file.write(template + ": " + str(value))
         file.close()
-        ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+        if notify:
+            ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+        else:
+            time.sleep(1)
     
+    if keyboard.is_pressed(keybindDown):
+        value -= countD
+        file = open("counter.txt", "w")
+        file.write(template + ": " + str(value))
+        file.close()
+        if notify:
+            ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+        else:
+            time.sleep(1)
