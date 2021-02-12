106b2408e18b721169de12b21b99ef7255631a74
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-02-12 16:32:58 +1100

    Added functionality

---
+++ main.py
@@ -0,0 +1,15 @@
+import keyboard
+from win10toast import ToastNotifier
+
+template = input("What are you counting? ")
+count = int(input("What is the increment value? "))
+value = 0
+
+while True:
+    if keyboard.is_pressed("ctrl+l"):
+        value += count
+        file = open("text.txt", "w")
+        file.write(template + ": " + str(value))
+        file.close()
+        ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+    
