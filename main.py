3c9b59f06c246cda4d484a381cdbdfbdde7b022b
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-21 15:24:08 +1100

    bug fixes

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ from win10toast import ToastNotifier
 def run(item, countI, countD, notify, keybindUp, keybindDown):
     value = 0
     while True:
-    if keyboard.is_pressed(keybindUp):
-        value += countI
-        file = open("counter.txt", "w")
-        file.write(item + ": " + str(value))
-        file.close()
-        if notify:
-            ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
-        else:
-            time.sleep(1)
-    
-    if keyboard.is_pressed(keybindDown):
-        value -= countD
-        file = open("counter.txt", "w")
-        file.write(item + ": " + str(value))
-        file.close()
-        if notify:
-            ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
-        else:
-            time.sleep(1)
+        if keyboard.is_pressed(keybindUp):
+            value += countI
+            file = open("counter.txt", "w")
+            file.write(item + ": " + str(value))
+            file.close()
+            if notify:
+                ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+            else:
+                time.sleep(1)
+        
+        if keyboard.is_pressed(keybindDown):
+            value -= countD
+            file = open("counter.txt", "w")
+            file.write(item + ": " + str(value))
+            file.close()
+            if notify:
+                ToastNotifier().show_toast("Added", "Total is: " + str(value), duration=1)
+            else:
+                time.sleep(1)
 
 
-If __name__ == "__main__":
+if __name__ == "__main__":
     template = input("What are you counting? ")
     print("")
     countI = int(input("What is the increment value? "))
@@ -36,7 +36,7 @@ If __name__ == "__main__":
     keybindUp = input("Please enter a keybind to increment: ")
     keybindDown = input("Please enter a keybind to decrement: ")
 
-    If notify.lower() == "yes" or notify.lower() == "y":
+    if notify.lower() == "yes" or notify.lower() == "y":
         notify = True
         print("a")
     else:
